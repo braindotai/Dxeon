c032202ee9bfa4882fd64617fb4113baf726aa9c
Rishik Mourya <braindotai@gmail.com>
2021-10-13 14:45:02 +0530

    fixed imports for json io

--- src/dxeon/io/json.py
+++ src/dxeon/io/json.py
@@ -8,11 +8,3 @@ def read(json_path: str) -> Dict:
 def write(json_path: str, content: Dict) -> Dict:
     with open(json_path, 'w') as file:
         json.dump(content, file)
-
-def read_bytes(json_path: str) -> Dict:
-    with open(json_path, 'rb') as file:
-        return json.load(file)
-
-def write_bytes(json_path: str) -> Dict:
-    with open(json_path, 'wb') as file:
-        return json.load(file)
