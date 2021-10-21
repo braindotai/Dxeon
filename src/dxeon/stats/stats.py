65fa47de83390742e3ba1e25fb6b47d22516af75
Rishik Mourya <braindotai@gmail.com>
2021-10-21 16:11:22 +0530

    fixed pre device issue

--- src/dxeon/stats/stats.py
+++ src/dxeon/stats/stats.py
@@ -2,10 +2,15 @@ from typing import List, Tuple, Union
 import torch
 from torch import nn
 from torchinfo import summary
+from ..utils.model import get_device
 
 def summarize(item: Union[nn.Module, torch.Tensor], input_size: Union[List, Tuple] = None, device: str = 'cpu', name = ''):
     if isinstance(item, nn.Module):
+        _original_device = get_device(item)
         summary(item, input_size = input_size, device = device)
+
+        if device != _original_device:
+            item.to(_original_device)
     
     elif isinstance(item, torch.Tensor):
         print(f'{name} Shape\t:', item.shape)
@@ -17,4 +22,4 @@ def summarize(item: Union[nn.Module, torch.Tensor], input_size: Union[List, Tupl
         print(f'{name} Sum\t:', item.sum().item())
         print(f'{name} Mean\t:', item.mean().item())
         print(f'{name} Std\t:', item.std().item())
-        print(f'{name} Median\t:', item.median().item())
+        print(f'{name} Median\t:', item.median().item(), '\n')
