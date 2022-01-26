052c38ffcfe812c3dd584f28bd8c3c24421819a3
Rishik Mourya <braindotai@gmail.com>
2022-01-26 14:31:03 +0530

    added ml utils

--- src/dxeon/stats/stats.py
+++ src/dxeon/stats/stats.py
@@ -7,10 +7,12 @@ from ..utils.model import get_device
 def summarize(item: Union[nn.Module, torch.Tensor], input_size: Union[List, Tuple] = None, device: str = 'cpu', name = ''):
     if isinstance(item, nn.Module):
         _original_device = get_device(item)
-        summary(item, input_size = input_size, device = device)
+        summary_ = summary(item, input_size = input_size, device = device)
 
         if device != _original_device:
             item.to(_original_device)
+        
+        return summary_
     
     elif isinstance(item, torch.Tensor):
         print(f'{name} Shape\t:', item.shape)
---
+++ src/dxeon/utils/ml.py
@@ -0,0 +1,30 @@
+from torch.utils.data import DataLoader
+from ...dxeon import DataLoader
+from ..stats import summarize
+
+def get_dataloaders(train_dataset, val_dataset = None, test_dataset = None, batch_size = 32, pin_memory = True, num_workers = 0):
+	train_dataloader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True, pin_memory = pin_memory, num_workers = num_workers)
+	
+	if val_dataset:
+		val_dataloader = DataLoader(val_dataset, batch_size = batch_size, shuffle = False, pin_memory = pin_memory, num_workers = num_workers)
+	
+	if test_dataset:
+		test_dataloader = DataLoader(test_dataset, batch_size = batch_size, shuffle = False, pin_memory = pin_memory, num_workers = num_workers)
+
+	if not test_dataset and not val_dataset:
+		return train_dataloader
+
+	if not test_dataset and val_dataset:
+		return train_dataloader, val_dataloader
+
+	if not val_dataset and test_dataset:
+		return train_dataloader, test_dataloader
+
+	return train_dataloader, val_dataloader, test_dataloader
+
+def summarize_batch(dataloader):
+	for batch in dataloader:
+		break
+	
+	for item in batch:
+		summarize(item)
